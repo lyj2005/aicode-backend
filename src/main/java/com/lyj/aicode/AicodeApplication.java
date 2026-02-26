472d54c2b056813de7391baec59150ab837f320b
lyj <820140968@qq.com>
2026-02-26 21:15:37 +0800

    1. 新建项目

---
+++ src/main/java/com/lyj/aicode/AicodeApplication.java
@@ -0,0 +1,13 @@
+package com.lyj.aicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AicodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AicodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lyj/aicode/AicodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyj.aicode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AicodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
