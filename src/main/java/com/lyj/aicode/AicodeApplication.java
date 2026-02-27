10953df317ce8780b761d8210ef2ac214128088b
lyj <820140968@qq.com>
2026-02-27 08:43:41 +0800

    2. 项目初始化

--- src/main/java/com/lyj/aicode/AicodeApplication.java
+++ src/main/java/com/lyj/aicode/AicodeApplication.java
@@ -1,9 +1,11 @@
 package com.lyj.aicode;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.lyj.aicode.mapper")
 public class AicodeApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/lyj/aicode/controller/HealthController.java
@@ -0,0 +1,21 @@
+package com.lyj.aicode.controller;
+
+
+import com.lyj.aicode.common.BaseResponse;
+import com.lyj.aicode.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HealthController {
+
+    /**
+     * 健康检查
+     */
+    @GetMapping("/health")
+    public BaseResponse<String> health() {
+        return ResultUtils.success("ok");
+    }
+}
